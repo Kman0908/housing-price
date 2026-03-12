c61335f70b0c482820c622a967b3c3965fd32555
Krish Sharma <krish9pro@gmail.com>
2026-03-13 02:16:24 +0530

    Data ingestion complete

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path :str = os.path.join('artifacts', 'raw.csv')
+    train_data_path :str = os.path.join('artifacts', 'train.csv')
+    test_data_path :str = os.path.join('artifacts', 'test.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_obj = DataIngestionConfig()
+
+    def start_data_ingestion(self):
+        logging.info('Data Ingestion Strated')
+        try:
+            data_path = os.path.join(os.getcwd(), 'data', 'train.csv')
+            df = pd.read_csv(data_path)
+            logging.info('Data read as pandas dataframe')
+
+            os.makedirs(os.path.dirname(self.data_ingestion_obj.raw_data_path), exist_ok = True)
+            df.to_csv(self.data_ingestion_obj.raw_data_path, index = False, header = True)
+
+            train_data, test_data = train_test_split(df, random_state = 42, test_size = 0.2)
+            train_data.to_csv(self.data_ingestion_obj.train_data_path, index = False, header = True)
+            test_data.to_csv(self.data_ingestion_obj.test_data_path, index = False, header = True)
+
+            logging.info('Ingestion complete')
+            return(
+                self.data_ingestion_obj.train_data_path,
+                self.data_ingestion_obj.test_data_path
+            )
+
+        except Exception as e:
+            logging.exception('Exception occurred in data ingestion')
+            raise CustomException(e, sys)
+        
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_path, test_path = obj.start_data_ingestion()
